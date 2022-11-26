47df302d47c93b7372cb0ebb5da90a81b536ffa3
Martin Beracochea <mbc@ebi.ac.uk>
2022-11-26 15:18:34 +0000

    Update containers, nf trace compatible now. Some to biocontainers.
    This branch has a the following:
    - Moved over to quay.io (dockerhub has pull limits now)
    - Migrated some tools to biocontainers, less maintaince work
    - Added the tools required for Nextflow monitoring and trace
    
    TODO: complete the build.py command to push all the containers.
    I would like to use gh actions to push the containers when
    we get a new release.

--- containers/build.py
+++ containers/build.py
@@ -9,13 +9,12 @@ import docker
 
 
 def main():
-    """Build the repo containers using Docker.
-    """
+    """Build the repo containers using Docker."""
     client = docker.from_env()
 
     for df_location in glob.glob("*/Dockerfile"):
         df_path = Path(df_location)
-        
+
         df_parsed = DockerfileParser(path=df_location)
 
         print("")
@@ -29,12 +28,23 @@ def main():
 
         try:
             image, build_log = client.images.build(
-                path=str(df_path.parent), rm=True, tag=f"quay.io/microbiome-informatics/{tool}:{version_tag}"
+                path=str(df_path.parent),
+                rm=True,
+                tag=f"quay.io/microbiome-informatics/{tool}:{version_tag}",
             )
             for line in build_log:
                 print(line.get("stream", "----"), end="")
+
+            client.images.push(
+                f"quay.io/microbiome-informatics/{tool}",
+                tag=f"quay.io/microbiome-informatics/{tool}:{version_tag}",
+            )
+
         except docker.errors.BuildError as build_ex:
-            print(f"Build of {tool} with tag:{version_tag} FAILED. Exception {build_ex}", file=sys.stderr)
+            print(
+                f"Build of {tool} with tag:{version_tag} FAILED. Exception {build_ex}",
+                file=sys.stderr,
+            )
             print(f"Build exception {build_ex}", file=sys.stderr)
             raise build_ex
 
