8fd977a95ef1f241fc0028ad87f0be0a1b398da6
Martin Beracochea <mbc@ebi.ac.uk>
2023-03-20 16:15:32 +0000

    Swap PyYAML for Ruamel.yaml on test_cwl_input_generator.py

--- tests/test_cwl_input_generator.py
+++ tests/test_cwl_input_generator.py
@@ -2,45 +2,57 @@
 
 import os
 import unittest
-import subprocess 
+import subprocess
 
 import bin.cwl_input as cwl_input
 
-import yaml
+from ruamel.yaml import YAML
+
+yaml = YAML()
 
 
 class CwlGeneration(unittest.TestCase):
-
     def _get_path(self):
         return os.path.abspath(
-            "/" + os.path.dirname(__file__)
-            + "/cwl_input_generation")
+            "/" + os.path.dirname(__file__) + "/cwl_input_generation"
+        )
 
     def test_cwl_generation(self):
-        """Assert that the .yml generation works.
-        """
+        """Assert that the .yml generation works."""
         result_file = self._get_path() + "/input_generated_basic.yml"
         args = [
-            "python", os.path.abspath(cwl_input.__file__),
-            "-i", "dummy_fasta.fasta",
-            "-f", "2.0",
-            "-s", "dummy_virsorter_dir",
-            "-d", "dummy_virfinder_model",
-            "-a", "dummy_hmms_tsv",
-            "-j", "dummy_hmmscan_db.hmm",
-            "-n", "dummy_nbci_db",
-            "-b", "dummy_img_db",
-            "-cv", "dummy_checkv_database_dir",
-            "-o", result_file
+            "python",
+            os.path.abspath(cwl_input.__file__),
+            "-i",
+            "dummy_fasta.fasta",
+            "-f",
+            "2.0",
+            "-s",
+            "dummy_virsorter_dir",
+            "-d",
+            "dummy_virfinder_model",
+            "-a",
+            "dummy_hmms_tsv",
+            "-j",
+            "dummy_hmmscan_db.hmm",
+            "-n",
+            "dummy_nbci_db",
+            "-b",
+            "dummy_img_db",
+            "-cv",
+            "dummy_checkv_database_dir",
+            "-o",
+            result_file,
         ]
         process = subprocess.run(args)
 
         assert process.returncode == 0
 
-        with open(result_file, "r") as rf_handler, \
-             open(self._get_path() + "/expected_basic.yml", "r") as e_handler:
-            produced_yaml = yaml.load(rf_handler, Loader=yaml.FullLoader)
-            expected_yaml = yaml.load(e_handler, Loader=yaml.FullLoader)
+        with open(result_file, "r") as rf_handler, open(
+            self._get_path() + "/expected_basic.yml", "r"
+        ) as e_handler:
+            produced_yaml = yaml.load(rf_handler)
+            expected_yaml = yaml.load(e_handler)
 
         # clean
         os.remove(result_file)
@@ -53,28 +65,42 @@ class CwlGeneration(unittest.TestCase):
         """
         result_file = self._get_path() + "/input_generated_full.yml"
         args = [
-            "python", os.path.abspath(cwl_input.__file__),
-            "-i", "dummy_fasta_second.fasta",
-            "-f", "0.7",
-            "-s", "dummy_virsorter_dir",
-            "-d", "dummty_virfinder_model",
-            "-a", "dummy_hmms_tsv",
-            "-j", "dummy_hmmscan_db.hmm",
-            "-n", "dummy_nbci_db",
-            "-b", "dummy_img_db",
-            "-cv", "dummy_checkv_database_dir",
-            "-v", "true",
-            "-m", "dummy_mashmap_ref",
-            "-o", result_file
+            "python",
+            os.path.abspath(cwl_input.__file__),
+            "-i",
+            "dummy_fasta_second.fasta",
+            "-f",
+            "0.7",
+            "-s",
+            "dummy_virsorter_dir",
+            "-d",
+            "dummty_virfinder_model",
+            "-a",
+            "dummy_hmms_tsv",
+            "-j",
+            "dummy_hmmscan_db.hmm",
+            "-n",
+            "dummy_nbci_db",
+            "-b",
+            "dummy_img_db",
+            "-cv",
+            "dummy_checkv_database_dir",
+            "-v",
+            "true",
+            "-m",
+            "dummy_mashmap_ref",
+            "-o",
+            result_file,
         ]
         process = subprocess.run(args)
 
         assert process.returncode == 0
 
-        with open(result_file, "r") as rf_handler, \
-             open(self._get_path() + "/expected_full.yml", "r") as e_handler:
-            produced_yaml = yaml.load(rf_handler, Loader=yaml.FullLoader)
-            expected_yaml = yaml.load(e_handler, Loader=yaml.FullLoader)
+        with open(result_file, "r") as rf_handler, open(
+            self._get_path() + "/expected_full.yml", "r"
+        ) as e_handler:
+            produced_yaml = yaml.load(rf_handler)
+            expected_yaml = yaml.load(e_handler)
 
         # clean
         os.remove(result_file)
