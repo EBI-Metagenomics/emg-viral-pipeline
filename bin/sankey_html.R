cca1e546b2f8826bb940be931952c72d2fbcf0b1
Ekaterina Sakharova <kates@ebi.ac.uk>
2025-04-04 14:25:48 +0100

    give perm

--- bin/sankey_html.R
+++ bin/sankey_html.R
@@ -1,12 +1,43 @@
 #!/usr/bin/env Rscript
 
-library(sankeyD3)
-library(magrittr)
+# Load necessary libraries
+suppressPackageStartupMessages({
+  library(sankeyD3)
+  library(magrittr)
+  library(jsonlite)
+})
 
-args = commandArgs(trailingOnly = TRUE)
+# Parse command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
 
-Taxonomy <- jsonlite::fromJSON(args[1])
+if (length(args) < 2) {
+  stop("Usage: script.R input.json output.html")
+}
 
-# print to HTML file
-sankey = sankeyNetwork(Links = Taxonomy\$links, Nodes = Taxonomy\$nodes, Source = "source", Target = "target", Value = "value", NodeID = "name", units = "count", fontSize = 22, nodeWidth = 30, nodeShadow = TRUE, nodePadding = 30, nodeStrokeWidth = 1, nodeCornerRadius = 10, dragY = TRUE, dragX = TRUE, numberFormat = ",.3g", align = "left", orderByPath = TRUE)
-saveNetwork(sankey, file = '${id}.sankey.html')
+# Read JSON input
+Taxonomy <- fromJSON(args[1])
+
+# Generate the Sankey diagram
+sankey <- sankeyNetwork(
+  Links = Taxonomy$links,
+  Nodes = Taxonomy$nodes,
+  Source = "source",
+  Target = "target",
+  Value = "value",
+  NodeID = "name",
+  units = "count",
+  fontSize = 22,
+  nodeWidth = 30,
+  nodeShadow = TRUE,
+  nodePadding = 30,
+  nodeStrokeWidth = 1,
+  nodeCornerRadius = 10,
+  dragY = TRUE,
+  dragX = TRUE,
+  numberFormat = ",.3g",
+  align = "left",
+  orderByPath = TRUE
+)
+
+# Save to HTML
+saveNetwork(sankey, file = args[2])
