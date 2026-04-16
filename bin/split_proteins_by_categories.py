7520ab4eda986c859dd7d47e087dc149785bd78f
Sofia Ochkalova <so.ochkalova@gmail.com>
2026-04-16 16:44:30 +0100

    remove nested for loop by using proteins_by_contig look up map

--- bin/split_proteins_by_categories.py
+++ bin/split_proteins_by_categories.py
@@ -18,6 +18,7 @@ import sys
 import argparse
 import re
 import logging
+from collections import defaultdict
 from copy import deepcopy
 from Bio import SeqIO
 
@@ -44,6 +45,7 @@ class SplitProteins:
         self.proteins = proteins
         self.output_file = output_file
         self.verbose = verbose
+        self.protein_id_re = re.compile(r"^(?P<contig_name>.+)_(?P<protein_number>\d+)$")
         self.setup_logging()
         self.logger = logging.getLogger(__name__)
 
@@ -93,47 +95,59 @@ class SplitProteins:
             return False
         
     def grep_proteins(self):
-        fna_sequences = SeqIO.parse(self.input_file, 'fasta')
+        contigs = SeqIO.parse(self.input_file, 'fasta')
         protein_sequences = SeqIO.parse(self.proteins, 'fasta')
         proteins = {}
+        proteins_by_contig = defaultdict(list)
         for seq in protein_sequences:
-            proteins[str(seq.id)] = seq
-        already_added_protein_ids = {}
+            protein_id = seq.id
+            proteins[protein_id] = seq
+            # Expected Prodigal protein id examples:
+            # ERZ21830300_185216_2719 # 3320783 # 3379543 # -1 # ID=195062_2719;partial=00;start_type=GTG;rbs_motif=None;rbs_spacer=None;gc_cont=0.731
+            # NODE_6_length_273677_cov_7.926969_6 # 2328 # 3188 # 1 # ID=1_6;partial=00;start_type=ATG;rbs_motif=GGA/GAG/AGG;rbs_spacer=5-10bp;gc_cont=0.237
+            match = self.protein_id_re.match(protein_id)
+            if not match:
+                raise ValueError(f"Invalid protein id format: {protein_id}")
+            contig_name = match.group('contig_name')
+            proteins_by_contig[contig_name].append(protein_id)
+
+        already_added_protein_ids = set()
+        written_records = 0
         with open(self.output_file, 'w') as out_file:
-            for fna in fna_sequences:
-                id, prophage_addition = None, None
-                contig_name = fna.id.split('|')[0]   # ex. NODE_3_length_498519_cov_223.607530
-                for i in proteins.keys():
-                    # Use startswith(contig_name + "_") rather than a plain substring
-                    # check ("contig_name in i") to avoid false matches between contigs
-                    # whose names share a numeric prefix, e.g. ERZ23434430_1 must not
-                    # match proteins from ERZ23434430_114 or ERZ23434430_759.
-                    if i.startswith(contig_name + "_"):
-                        # if prophage
-                        if len(fna.id.split('|')) > 1:  # ex. NODE_3_length_498519_cov_223.607530|prophage-81521:94004 or NODE_3_length_498519_cov_223.607530|phage-circular
-                            prophage_addition = fna.id.split('|')[1]  # prophage-81521:94004 or phage-circular
-                        id = i
-                        already_added_protein_ids.setdefault(id, 0)
-                        record = deepcopy(proteins[id])  # ex. >NODE_6_length_273677_cov_7.926969_6 # 2328 # 3188 # 1 # ID=1_6;partial=00;start_type=ATG;rbs_motif=GGA/GAG/AGG;rbs_spacer=5-10bp;gc_cont=0.237
-                        protein_info = record.description.split(contig_name)[1]  #_6 # 2328 # 3188 # 1 # ID=1_6;partial=00;start_type=AT etc
-                        protein_number = protein_info.split(' ')[0]
-                        """
-                        If we want to simulate proper prodigal work we need to change proteinID 
-                        to new numbers starting with 1. Otherwise we have some skipping values from original faa
-                        """
-                        if prophage_addition:
-                            if not self.check_coordinates(protein_info, prophage_addition):
-                                continue
-                            record.description = f'{contig_name}|{prophage_addition}{protein_info}'
-                            record.id = f'{contig_name}|{prophage_addition}{protein_number}'
-                            already_added_protein_ids[id] += 1
-                        SeqIO.write(record, out_file, "fasta")
-                if not id:
+            for contig in contigs:
+                contig_name = contig.id.split('|')[0]   # ex. NODE_3_length_498519_cov_223.607530
+                contig_id_parts = contig.id.split('|', 1)
+                prophage_addition = contig_id_parts[1] if len(contig_id_parts) > 1 else None
+
+                matching_protein_ids = proteins_by_contig.get(contig_name, [])
+                if not matching_protein_ids:
                     self.logger.info(f'No proteins found for {contig_name}')
-        # for checking - that should not output anything
-        for i in already_added_protein_ids:
-            if already_added_protein_ids[i] > 1:
-                self.logger.info(f'Added twice {i}')
+                
+                for protein_id in matching_protein_ids:
+                    record = deepcopy(proteins[protein_id])  # ex. >NODE_6_length_273677_cov_7.926969_6 # 2328 # 3188 # 1 # ID=1_6;partial=00;start_type=ATG;rbs_motif=GGA/GAG/AGG;rbs_spacer=5-10bp;gc_cont=0.237
+                    protein_info = record.description.split(contig_name)[1]  # 2328 # 3188 # 1 # ID=1_6;partial=00;start_type=ATG;rbs_motif=GGA/GAG/AGG;rbs_spacer=5-10bp;gc_cont=0.237
+                    protein_number = protein_info.split(' ')[0]
+                    # TODO: If we want to simulate proper Prodigal naming scheme we need to change proteinID
+                    # to new numbers starting with 1. Otherwise we have some skipped numbers in the output fasta
+                    if prophage_addition:
+                        if not self.check_coordinates(protein_info, prophage_addition):
+                            continue
+                        record.description = f'{contig_name}|{prophage_addition}{protein_info}'
+                        record.id = f'{contig_name}|{prophage_addition}{protein_number}'
+                    
+                    if protein_id in already_added_protein_ids:
+                        raise RuntimeError(f"Protein added more than once: {protein_id}")
+                    
+                    SeqIO.write(record, out_file, "fasta")
+                    already_added_protein_ids.add(protein_id)
+                    written_records += 1
+
+        if written_records == 0:
+            raise RuntimeError(
+                'No proteins matched contigs from input fasta. '
+                'Check contig naming between input fasta and proteins fasta.'
+            )
+
 
 def main():
     args = parse_args(sys.argv[1:])
